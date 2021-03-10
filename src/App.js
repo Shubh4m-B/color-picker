14ba4cbe67e341c581d7954b84e3a3c90115deb2
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-10 18:17:43 +0530

    Add colorHelper to generate color shades in rgb,rgba using chroma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Palette from './Palette'
 import seedColors from './seedColors'
+import {generatePalette} from './ColorHelper';
 import './App.css'
 
 export class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[7]));
     return (
       <div className="App">
         <Palette {...seedColors[7]}/>
