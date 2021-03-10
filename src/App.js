ee2d3cdd0bb2a0afd56a16742cab8db7290d1683
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-10 12:50:52 +0530

    Add copy functionality and animation on copy to colorBox components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Palette from './Palette'
 import seedColors from './seedColors'
+import './App.css'
 
 export class App extends Component {
   render() {
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
+import {CopyToClipboard} from "react-copy-to-clipboard";
+
 
 import './ColorBox.css'
 
 export class ColorBox extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            copied:false
+        };
+        this.changeCopySate = this.changeCopySate.bind(this);
+    }
+
+    changeCopySate(){
+        this.setState({
+            copied:true
+        },()=>{
+            setTimeout(()=>this.setState({
+                copied:false
+            }), 1500);
+        })
+    }
     render() {
         const {name, background} = this.props;
+        const {copied} = this.state;
         return (
-            <div style={{background: background}} className="ColorBox">
-                <div className="ColorBox-copy-container">
-                    <div className="Colorbox-content">
-                        <span>{name}</span>
+            <CopyToClipboard text={background} onCopy={this.changeCopySate}>
+                <div style={{background: background}} className="ColorBox">
+                    <div className={`ColorBox-overlay ${copied && "show"}`} style={{background:background}}></div>
+                    <div className={`ColorBox-copymsg ${copied && "show"}`}>
+                        <h1>Copied!</h1>
+                        <p>{background}</p>
                     </div>
-                    <button className="ColorBox-copyButton">Copy</button>
+                    <div className="ColorBox-copy-container">
+                        <div className="Colorbox-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="ColorBox-copyButton">Copy</button>
+                    </div>
+                    <span className="ColorBox-more">More</span>
                 </div>
-                <span className="ColorBox-more">More</span>
-            </div>
+            </CopyToClipboard>
         )
     }
 }
