e0df99ef16431a8aab9bde2edc7f34f30e44dc1a
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-12 12:01:14 +0530

    Add route for individual color

--- src/App.js
+++ src/App.js
@@ -16,13 +16,22 @@ export class App extends Component {
     console.log(generatePalette(seedColors[7]))
     return (
       <Switch>
-        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
+        <Route
+          exact
+          path="/"
+          render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />}
+        />
         <Route
           exact
           path="/palette/:id"
           render={routeProps => (
             <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
           )}
+        />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>Single color component</h1>}
         />
       </Switch>
       // <div className="App">
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react'
-import {CopyToClipboard} from "react-copy-to-clipboard";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from "react-router-dom"
 
 
 import './ColorBox.css'
 
 export class ColorBox extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            copied:false
+            copied: false
         };
         this.changeCopySate = this.changeCopySate.bind(this);
     }
 
-    changeCopySate(){
+    changeCopySate() {
         this.setState({
-            copied:true
-        },()=>{
-            setTimeout(()=>this.setState({
-                copied:false
+            copied: true
+        }, () => {
+            setTimeout(() => this.setState({
+                copied: false
             }), 1500);
         })
     }
     render() {
-        const {name, background} = this.props;
-        const {copied} = this.state;
+        const { name, background } = this.props;
+        const { copied } = this.state;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopySate}>
-                <div style={{background: background}} className="ColorBox">
-                    <div className={`ColorBox-overlay ${copied && "show"}`} style={{background:background}}></div>
+                <div style={{ background: background }} className="ColorBox">
+                    <div className={`ColorBox-overlay ${copied && "show"}`} style={{ background: background }}></div>
                     <div className={`ColorBox-copymsg ${copied && "show"}`}>
                         <h1>Copied!</h1>
                         <p>{background}</p>
@@ -39,7 +40,9 @@ export class ColorBox extends Component {
                         </div>
                         <button className="ColorBox-copyButton">Copy</button>
                     </div>
-                    <span className="ColorBox-more">More</span>
+                    <Link to="/" onClick={e => e.stopPropagation()}>
+                        <span className="ColorBox-more">More</span>
+                    </Link>
                 </div>
             </CopyToClipboard>
         )
