79f14e9d66f41a073215adfb57b671a251c805e3
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-12 19:52:44 +0530

    Adjusting text color in colorboxes according to background luminence using chroma

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import chroma from 'chroma-js';
 
 import './ColorBox.css'
 
@@ -26,23 +26,27 @@ export class ColorBox extends Component {
     render() {
         const { name, background, url, showLink } = this.props;
         const { copied } = this.state;
+
+        const isDarkColor = chroma(background).luminance() < 0.075;
+        const isLightColor = chroma(background).luminance() >= 0.075;
+
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopySate}>
                 <div style={{ background: background }} className="ColorBox">
                     <div className={`ColorBox-overlay ${copied && "show"}`} style={{ background: background }}></div>
                     <div className={`ColorBox-copymsg ${copied && "show"}`}>
                         <h1>Copied!</h1>
-                        <p>{background}</p>
+                        <p className={isLightColor && "dark-text"}>{background}</p>
                     </div>
                     <div className="ColorBox-copy-container">
                         <div className="Colorbox-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor && "light-text"}>{name}</span>
                         </div>
-                        <button className="ColorBox-copyButton">Copy</button>
+                        <button className={`ColorBox-copyButton ${isLightColor && "dark-text"}`}>Copy</button>
                     </div>
                     {showLink && (
                         <Link to={url} onClick={e => e.stopPropagation()}>
-                            <span className="ColorBox-more">More</span>
+                            <span className={`ColorBox-more ${isLightColor && "dark-text"}`}>More</span>
                         </Link>
                     )}
                 </div>
