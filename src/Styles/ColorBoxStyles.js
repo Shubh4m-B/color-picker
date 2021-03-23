4fc68dcfed943954e22be46d2da048b71460ec00
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-23 21:02:55 +0530

    Make colorboxes fully responsive

--- src/Styles/ColorBoxStyles.js
+++ src/Styles/ColorBoxStyles.js
@@ -14,11 +14,11 @@ export default {
         },
         [sizes.down("lg")]: {
             width: "25%",
-            height: props => (props.showingFullPalette ? "20%" : "50%")
+            height: props => (props.showingFullPalette ? "20%" : "33.3333%")
         },
         [sizes.down("md")]: {
             width: "50%",
-            height: props => (props.showingFullPalette ? "10%" : "50%")
+            height: props => (props.showingFullPalette ? "10%" : "20%")
         },
         [sizes.down("xs")]: {
             width: "100%",
@@ -117,7 +117,10 @@ export default {
             marginBottom: "0",
             padding: "1rem",
             textTransform: "uppercase",
-            fontSize: "5rem"
+            fontSize: "5rem",
+            [sizes.down("xs")]: {
+                fontSize: "4rem"
+            }
         },
         "& p": {
             fontSize: "2rem",
