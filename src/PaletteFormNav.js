57f10f75888209e69f8d381542b823b996754fc2
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-23 12:59:43 +0530

    Add minor style updates to newpaletteform

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -7,7 +7,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Button from '@material-ui/core/Button';
 import { ValidatorForm } from 'react-material-ui-form-validator';
 import PaletteMetaForm from './PaletteMetaForm';
@@ -41,7 +41,7 @@ export class PaletteFormNav extends Component {
                             edge="start"
                             className={clsx(classes.menuButton, open && classes.hide)}
                         >
-                            <MenuIcon />
+                            <ChevronRightIcon />
                         </IconButton>
                         <Typography variant="h6" noWrap>
                             Create New Palette
--- src/Styles/NewPaletteFormStyles.js
+++ src/Styles/NewPaletteFormStyles.js
@@ -25,11 +25,12 @@ const useStyles = makeStyles((theme) => ({
         // necessary for content to be below app bar
         ...theme.mixins.toolbar,
         justifyContent: 'flex-end',
+        width: "100%"
     },
     content: {
         flexGrow: 1,
         height: "calc(100vh - 64px)",
-        padding: theme.spacing(3),
+        padding: "0",
         transition: theme.transitions.create('margin', {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
