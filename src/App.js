c635688edcbf2045768be0761ea042323593ddcf
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-23 13:31:51 +0530

    Adding data persistence by saving palettes to local storage

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ import './App.css'
 export class App extends Component {
   constructor(props) {
     super(props);
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
     this.state = {
-      palettes: seedColors
+      palettes: savedPalettes || seedColors
     }
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
@@ -26,8 +27,13 @@ export class App extends Component {
 
   savePalette(newPalette) {
     this.setState(
-      { palettes: [...this.state.palettes, newPalette] }
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
     )
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes))
   }
 
   render() {
