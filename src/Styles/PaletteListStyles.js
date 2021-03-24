b808d0aeceb4cf3ba0e00c1a39d51fd27cf94946
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-24 13:48:57 +0530

    Make paletteList responsive

--- src/Styles/PaletteListStyles.js
+++ src/Styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './Sizes';
+
 export default {
     root: {
         backgroundColor: "blue",
@@ -8,11 +10,17 @@ export default {
 
     },
     container: {
-        width: "50%",
+        width: "60%",
         display: "flex",
         alignItems: "flex-start",
         flexDirection: "column",
         flexWrap: "wrap",
+        [sizes.down("lg")]: {
+            width: "70%"
+        },
+        [sizes.down("xs")]: {
+            width: "60%"
+        }
 
     },
     nav: {
@@ -31,6 +39,14 @@ export default {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3,30%)",
-        gridGap: "5%"
+        gridGap: "1.5rem",
+        [sizes.down("md")]: {
+            gridTemplateColumns: "repeat(2,50%)",
+
+        },
+        [sizes.down("xs")]: {
+            gridTemplateColumns: "repeat(1,100%)",
+
+        }
     }
 }
