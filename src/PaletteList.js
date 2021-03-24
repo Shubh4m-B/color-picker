7ab342a615c5bf69334e5a3f0a5da79f7273e874
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-03-24 20:39:10 +0530

    Add delete palette transition from react transition group

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withStyles } from "@material-ui/styles";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import MiniPalette from './MiniPalette';
 import styles from './Styles/PaletteListStyles';
 
@@ -19,11 +20,15 @@ export class PaletteList extends Component {
                         <h1 className={classes.heading}>Color Picker</h1>
                         <Link to="/palette/new">Create New Palette</Link>
                     </nav>
-                    <div className={classes.palettes}>
+
+                    <TransitionGroup className={classes.palettes}>
                         {palettes.map(palette => (
-                            <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} deletePalette={deletePalette} key={palette.id} id={palette.id} />
+                            <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+                                <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} deletePalette={deletePalette} key={palette.id} id={palette.id} />
+                            </CSSTransition>
                         ))}
-                    </div>
+                    </TransitionGroup>
+
                 </div>
             </div>
         )
